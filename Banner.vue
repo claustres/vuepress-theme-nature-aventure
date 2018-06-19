60dc3d3f6f5aa40fd3200a67d51bd6f738205a0b
Luc Claustres <luc.claustres@orange.fr>
2018-06-19 14:49:31 +0200

    Create Banner.vue

---
+++ Banner.vue
@@ -0,0 +1,12 @@
+<template>
+  <p align="center">
+    <i><small>Activités de pleine nature à Castelnaudary</small></i>
+  </p>
+</template>
+
+<script>
+</script>
+</template>
+
+<script>
+</script>
