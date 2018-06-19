d98d02b1300ae3ae6817c3310da3de7c998b9759
Luc Claustres <luc.claustres@orange.fr>
2018-06-19 14:46:56 +0200

    Update Layout.vue

--- Layout.vue
+++ Layout.vue
@@ -1,14 +1,17 @@
 <template>
   <Layout>
+    <Banner slot="sidebar-top"/>
   </Layout>
 </template>
 
 <script>
 import Layout from '@default-theme/Layout.vue'
+import Banner from './Banner.vue'
 
 export default {
   components: {
-    Layout
+    Layout,
+    Banner
   }
 }
 </script>
